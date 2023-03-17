2c84a5ee5a92627632d0b41930d7a5b454b88fcc
NicolasLeymat <74655132+NicolasLeymat@users.noreply.github.com>
2023-03-17 09:54:38 +0100

    Update ServeurBlog.php

--- php/ServeurBlog.php
+++ php/ServeurBlog.php
@@ -2,6 +2,7 @@
     /// Librairies éventuelles (pour la connexion à la BDD, etc.)
     include('./jwt_utils.php');
     require('./bdConnect.php');
+    include('./utils.php');
     
     /// Paramétrage de l'entête HTTP (pour la réponse au Client)
     header("Content-Type:application/json");
@@ -21,7 +22,7 @@
         echo 'Non Sauce Chibre';
     }
     else {
-
+        $tokenData = get_payload($token);
         /// Identification du type de méthode HTTP envoyée par le client
         switch ($http_method){
             /// Cas de la méthode GET
@@ -43,13 +44,21 @@
                         $reqLike->execute();
                         $nbLike = $reqLike->fetch();
                         
+
+                        if($tokenData['role'] == 'Anonymous'){
+                            $a = array( "id_article" => $articles['id_article'],
+                                        "Titre" => "".$articles['titre'],
+                                        "NomAut" => "".$utilisateur['identifiant'],
+                                        "Publi" => "".$articles['date_publication']);
+                        }else{
+                            $a = array( "id_article" => $articles['id_article'],
+                            "Titre" => "".$articles['titre'],
+                            "NomAut" => "".$utilisateur['identifiant'],
+                            "Publi" => "".$articles['date_publication'], 
+                            "NbLike" => "".$nbLike['nbLike'], 
+                            "NbDislike" => "".$nbDislike['nbDislike']);
+                        }
                         //Titre, NomAut, Publi, NbLike, NbDislike
-                        $a = array( "id_article" => $articles['id_article'],
-                                    "Titre" => "".$articles['titre'],
-                                    "NomAut" => "".$utilisateur['identifiant'],
-                                    "Publi" => "".$articles['date_publication'], 
-                                    "NbLike" => "".$nbLike['nbLike'], 
-                                    "NbDislike" => "".$nbDislike['nbDislike']);
                         
                         array_push($matchingData,$a); 
                     }
@@ -60,46 +69,78 @@
                 break;
             /// Cas de la méthode POST
             case "POST" :
-                /// Récupération des données envoyées par le Client
-
-                // $stmt = $mysqlConnection->prepare("SELECT MAX(Id) AS max_id FROM chuckn_facts");
-                // $stmt -> execute();
-                // $invNum = $stmt -> fetch(PDO::FETCH_ASSOC);
-                // $max_id = $invNum['max_id']+1;
-
-                // $postedData = file_get_contents('php://input');
-                // $res = json_decode($postedData, true);
-                // $req = $mysqlConnection->prepare('INSERT INTO chuckn_facts values
-                // (:id,:val, 0, "2023-02-01 08:00:00", NULL, 0, 0)');
-                // $req->bindParam(':id',$max_id,PDO::PARAM_INT);
-                // $req->bindParam(':val',$res['phrase'],PDO::PARAM_STR);
-                // $res = $req->execute();
-                // echo $res;
-                // if (!$res){
-                //     die('Aled');
-                // }
-                // /// Traitement
-                // /// Envoi de la réponse au Client
-                // deliver_response(201, "Votre message", NULL);
+                $postedData = file_get_contents('php://input');
+                $res = json_decode($postedData, true);
+                $commande = insert_article($res['titre'], $res['contenu'], $res['id_user']);
+                switch($commande){
+                    case 1:
+                        deliver_response(201, "Creation de l'article", NULL);
+                        break;
+                    case -1:
+                        deliver_response(422, "une erreur est presente dans les données passé", NULL);
+                        break;
+                }
                 break;
                 /// Cas de la méthode PUT
             case "PUT" :
                 /// Récupération des données envoyées par le Client
-                // $postedData = file_get_contents('php://input');
-                // $res = json_decode($postedData, true);
-                // echo $res['phrase'];
-                // echo $_GET['id'];
-                // $req = $mysqlConnection->prepare('UPDATE chuckn_facts SET
-                // phrase = :phrase WHERE id = :id');
-                // $req->bindParam(':phrase', $res['phrase'],PDO::PARAM_STR);
-
-                // $req->bindParam(':id',$_GET['id'],PDO::PARAM_INT);
-                // $res = $req->execute();
-                // /// Traitement
-                // /// Envoi de la réponse au Client
-                // deliver_response(200, "Votre message", NULL);
+                $postedData = file_get_contents('php://input');
+                $res = json_decode($postedData, true);
+                echo $res['type'];
+                switch($res['type']){
+                    case 0:
+                        $commande =  update_article($res['id_article'], $res['contenu']);
+                        switch($commande){
+                            case 1:
+                                deliver_response(201, "Modification de l'article", NULL);
+                                break;
+                            case -1:
+                                deliver_response(422, "une erreur est presente dans les données passé", NULL);
+                                break;
+                        }
+                        break;
+                    case 1 : 
+                        $commande = like($res['id_utilisateur'], $res['id_article']);
+                        switch($commande){
+                            case 1:
+                                deliver_response(201, "Like effectué", NULL);
+                                break;
+                            case -1:
+                                deliver_response(422, "une erreur est presente dans les données passé", NULL);
+                                break;
+                        }
+                        break;
+                    case 2 : 
+                            $commande = dislike($res['id_utilisateur'], $res['id_article']);
+                            switch($commande){
+                                case 1:
+                                    deliver_response(201, "dislike effectué", NULL);
+                                    break;
+                                case -1:
+                                    deliver_response(422, "une erreur est presente dans les données passé", NULL);
+                                    break;
+                            }
+                            break;
+                }
                 break;
             /// Cas de la méthode DELETE
+            case "DELETE" : 
+                $postedData = file_get_contents('php://input');
+                $res = json_decode($postedData, true);
+                switch($res['type']){
+                    case 0:
+                        $commande =  delete_article($res['id_article']);
+                        switch($commande){
+                            case 1:
+                                deliver_response(201, "Suppression de l'article reussi", NULL);
+                                break;
+                            case -1:
+                                deliver_response(422, "une erreur est presente dans les données passé", NULL);
+                                break;
+                        }
+                        break;
+                }
+                break;
             default :
                 /// Récupération de l'identifiant de la ressource envoyé par le Client
                 // if (!empty($_GET['id'])){
